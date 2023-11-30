ae2854eaa157dde24d98e56c707489b3575a4996
J M Adis <jessicaadis7@gmail.com>
2023-11-30 12:49:00 +0000

    getting it ready to push

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,27 @@
 import "./styles/style.scss";
 import pokemonArray from "./data/pokemon";
 
-console.log(pokemonArray)
-
 const cardContainer = document.querySelector<HTMLElement>(".card-container");
 
 if (!cardContainer){
     throw new Error ("Issue with the selector of our container")
 }
 
-const displayPokemon = (pokemon) => {
+const displayPokemon = (pokemon : Pokemon) => {
     const card = document.createElement("div");
     card.classList.add("pokemon-card");
 
     card.innerHTML = `
-    <p>${pokemon.id}</p>
-    <p>${pokemon.name}</p>
-    <p> ${pokemon.types.join(",")}</p>
-    <img src="${pokemon.sprite}" alt="${pokemon.name}">
+    <img class="card__image" src="${pokemon.sprite}" alt="${pokemon.name}">
+    <div class="card__content">
+    <h2 style=text-transform:capitalize class="card__heading">${pokemon.name}</h2>
+    <p class="card__text"> ${pokemon.name} is a ${pokemon.types.join(" & ")} type pokemon. </p>
+    </div>
     `;
 
     cardContainer.appendChild(card);
+
+    card.style.backgroundColor = "#ffb7b7";
 }
 
 pokemonArray.forEach(displayPokemon)
