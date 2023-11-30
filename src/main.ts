0e038cc24f9c19e4193bd11334cfad70256f6fe6
J M Adis <jessicaadis7@gmail.com>
2023-11-30 11:13:18 +0000

    created the cards

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
 import "./styles/style.scss";
+import pokemonArray from "./data/pokemon";
 
-console.log("Hello World!")
+console.log(pokemonArray)
+
+const cardContainer = document.querySelector<HTMLElement>(".card-container");
+
+if (!cardContainer){
+    throw new Error ("Issue with the selector of our container")
+}
+
+const displayPokemon = (pokemon) => {
+    const card = document.createElement("div");
+    card.classList.add("pokemon-card");
+
+    card.innerHTML = `
+    <p>${pokemon.id}</p>
+    <p>${pokemon.name}</p>
+    <p> ${pokemon.types.join(",")}</p>
+    <img src="${pokemon.sprite}" alt="${pokemon.name}">
+    `;
+
+    cardContainer.appendChild(card);
+}
+
+pokemonArray.forEach(displayPokemon)
